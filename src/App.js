fdf80027af0b25c72f49f216384cca93764795dd
sungjinchoi <csj7480@gmail.com>
2021-03-25 21:44:07 +0900

    Finish adding array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Hello from './Hello';
 import './App.css';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 // import Wrapper from './Wrapper'
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username : '',
+    email : '',
+  });
+  const {username , email} = inputs;
+  const onChange = e => {
+    const {name , value } = e.target;
+    setInputs({
+      ...inputs,
+      [name] : value
+    })
+  }
+  const [users,setUsers] = useState([
+    {
+        id : 1,
+        username : 'sungjin',
+        email : 'csj7480@naver.com'
+    },
+    {
+        id : 2,
+        username : 'eunji',
+        email : 'dmswl2223@naver.com'
+    }
+]);
+
+const nextId = useRef(3);
+
+const onCreate = () =>{
+  const user = {
+    id : nextId.current,
+    ...inputs
+  };
+  setUsers(users.concat(user));
+  setInputs({
+    username : '',
+    email : ''
+  });
+  nextId.current += 1;
+}
+
   return (
-    <InputSample/>
+    <> 
+      <CreateUser 
+        username = {username}
+        email = {email}
+        onChange = {onChange}
+        onCreate = {onCreate} />
+      <UserList users = {users}/>
+    </>
   );
 }
 
---
+++ src/UserList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function User({user}){
+    return(
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    )
+}
+
+function UserList({ users }){
+    return(
+        <div>
+            {
+                users.map(
+                    (user,index) => (<User user = {user} key = {index}/>)
+                )
+            }
+                
+        </div>
+    )
+}
+
+export default UserList;
